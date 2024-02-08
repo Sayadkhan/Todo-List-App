2d6b13a6b2117df264596583e93f86d0a35d71ce
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2024-02-09 01:34:12 +0600

    complted and incomplete button added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,28 +7,26 @@ const Todo = ({ todo, toggleComplete, deleteTodo, editTodo }) => {
   return (
     <div className="bg-gray-900 flex justify-between w-full mt-5 py-10 px-3">
       {/* <p>{todo?.task}</p> */}
+
+      <div className="flex flex-col">
+        {todo.completed ? (
+          <div className="flex flex-col ">
+            <span className=" font-bold text-lg">{todo.task}</span>
+            <span className="text-sm text-green-500">Task is completed</span>
+          </div>
+        ) : (
+          <div className="flex flex-col">
+            <span className="font-bold text-lg">{todo.task}</span>
+            <span className="text-sm text-red-500">Task is incomplete</span>
+          </div>
+        )}
+      </div>
+
       <div className="flex gap-5">
         <button onClick={() => toggleComplete(todo.id)}>
           {todo.completed ? <GrCompliance /> : <RxCross1 />}
         </button>
 
-        <div className="flex flex-col">
-          {todo.completed ? (
-            <div className="flex flex-col ">
-              <span className="line-through decoration-[3px] decoration-red-500 font-bold text-lg">
-                {todo.task}
-              </span>
-              <span className="text-sm text-green-500">Task is completed</span>
-            </div>
-          ) : (
-            <div className="flex flex-col">
-              <span className="font-bold text-lg">{todo.task}</span>
-              <span className="text-sm text-red-500">Task is incomplete</span>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="flex gap-5">
         <button
           className="hover:text-green-500 duration-300"
           onClick={() => editTodo(todo.id)}
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -5,6 +5,10 @@ import EditTodoForm from "./EditTodoForm";
 
 const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
+  // const [completeTask, setCompleteTask] = useState([]);
+  // console.log(completeTask);
+
+  const [isCompletedSceen, setIsCompletedSceen] = useState(null);
 
   useEffect(() => {
     const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -17,7 +21,7 @@ const TodoWrapper = () => {
   // }));
 
   const completedTask = todos.filter((todo) => todo.completed === true);
-  console.log(completedTask);
+  const inCompletedTask = todos.filter((todo) => todo.completed === false);
 
   const addTodo = (todo) => {
     const newTodos = [
@@ -63,29 +67,122 @@ const TodoWrapper = () => {
   return (
     <div className="flex flex-col  justify-center  p-10">
       <div className="flex justify-between">
-        <div className="flex gap-5 my-5">
+        <div className="flex gap-3 my-5">
           <span>Total Task : </span>
           <span>{todos.length}</span>
         </div>
-        <div className="flex gap-5 my-5">
+        <div className="flex gap-3 my-5">
           <span>Completed Task : </span>
           <span>{completedTask.length}</span>
         </div>
       </div>
       <TodoForm addTodo={addTodo} />
+      <div className=" flex flex-col  md:flex md:flex-row  items-center justify-center gap-5 mt-5">
+        <button
+          className={`bg-teal-900/30 py-2 px-5 border-2 border-teal-500 rounded text-teal-500 hover:bg-teal-500 hover:text-gray-900 duration-300 w-full ${
+            isCompletedSceen === null && "active bg-teal-500 text-gray-900"
+          }`}
+          onClick={() => setIsCompletedSceen(null)}
+        >
+          All Todo
+        </button>
+        <button
+          className={`bg-teal-900/30 py-2 px-5 border-2 border-teal-500 rounded text-teal-500 hover:bg-teal-500 hover:text-gray-900 duration-300 w-full ${
+            isCompletedSceen === false && "active bg-teal-500 text-gray-900"
+          }`}
+          onClick={() => setIsCompletedSceen(false)}
+        >
+          incomplete Task
+        </button>
+        <button
+          className={`bg-teal-900/30 py-2 px-5 border-2 border-teal-500 rounded text-teal-500 hover:bg-teal-500 hover:text-gray-900 duration-300 w-full ${
+            isCompletedSceen === true &&
+            "active active bg-teal-500 text-gray-900"
+          }`}
+          onClick={() => setIsCompletedSceen(true)}
+        >
+          Completed Task
+        </button>
+      </div>
 
-      {todos.map((todo) =>
-        todo.isEditing ? (
-          <EditTodoForm key={todo.id} editTask={editTask} todo={todo} />
-        ) : (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            deleteTodo={deleteTodo}
-            editTodo={editTodo}
-            toggleComplete={toggleComplete}
-          />
-        )
+      {isCompletedSceen === null ? (
+        <div>
+          {todos.length > 0 ? (
+            todos.map((todo) =>
+              todo.isEditing ? (
+                <EditTodoForm key={todo.id} editTask={editTask} todo={todo} />
+              ) : (
+                <Todo
+                  key={todo.id}
+                  todo={todo}
+                  deleteTodo={deleteTodo}
+                  editTodo={editTodo}
+                  toggleComplete={toggleComplete}
+                />
+              )
+            )
+          ) : (
+            <div className="flex items-center justify-center my-5">
+              <p>There is no todo</p>
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          {!isCompletedSceen ? (
+            <div>
+              {inCompletedTask.length > 0 ? (
+                inCompletedTask.map((todo) =>
+                  todo.isEditing ? (
+                    <EditTodoForm
+                      key={todo.id}
+                      editTask={editTask}
+                      todo={todo}
+                    />
+                  ) : (
+                    <Todo
+                      key={todo.id}
+                      todo={todo}
+                      deleteTodo={deleteTodo}
+                      editTodo={editTodo}
+                      toggleComplete={toggleComplete}
+                    />
+                  )
+                )
+              ) : (
+                <div className="flex items-center justify-center my-5">
+                  <p>There is no incomplete task </p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div>
+              {completedTask.length > 0 ? (
+                completedTask.map((todo) =>
+                  todo.isEditing ? (
+                    <EditTodoForm
+                      key={todo.id}
+                      editTask={editTask}
+                      todo={todo}
+                    />
+                  ) : (
+                    <Todo
+                      key={todo.id}
+                      todo={todo}
+                      deleteTodo={deleteTodo}
+                      editTodo={editTodo}
+                      toggleComplete={toggleComplete}
+                    />
+                  )
+                )
+              ) : (
+                <div className="flex items-center justify-center my-5">
+                  <p>There is no complete task </p>
+                </div>
+              )}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
